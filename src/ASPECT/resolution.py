4e939e4db8a5420d1464624b8b7b04b8404558ab
Ananyo Bhattacharya <ananyo@csrwks2022-0092.engin.umich.edu>
2025-07-05 14:59:52 -0400

    Minor changes

--- src/ASPECT/resolution.py
+++ src/ASPECT/resolution.py
@@ -30,7 +30,7 @@ d = logspace(2, 6, 100)
 #For VIS
 aVIS = 10*pi/180
 pxVIS = 1024
-pVIS = d*aVIS/pxVIS 
+pVIS = d*tan(aVIS/(2*pxVIS))
 
 #For NIR-1 and NIR-2
 
@@ -39,8 +39,8 @@ aNIR2 = 5.4*pi/180
 pxNIR1 = 640
 pxNIR2 = 512
 
-pNIR1 = d*aNIR1/pxNIR1
-pNIR2 = d*aNIR2/pxNIR2
+pNIR1 = 2*d*tan(aNIR1/(pxNIR1*2))
+pNIR2 = 2*d*tan(aNIR2/(pxNIR2*2))
 
 
 #Hera Milani numbers for validation - TBD
@@ -48,11 +48,11 @@ pNIR2 = d*aNIR2/pxNIR2
 #Size of the TCO
 fig, ax = plt.subplots()
 ax.plot(d/1e3, pVIS, 'k')
-ax.plot(d/1e3, pNIR1, 'k-')
+ax.plot(d/1e3, pNIR1, 'k-.')
 ax.plot(d/1e3, pNIR2, 'k--')
-ax.plot(d/1e3, aVIS*d, 'b')
-ax.plot(d/1e3, aNIR1*d, 'b-')
-ax.plot(d/1e3, aNIR2*d, 'b--')
+ax.plot(d/1e3, tan(aVIS/2)*d*2, 'b')
+ax.plot(d/1e3, tan(aNIR1/2)*d*2, 'b-.')
+ax.plot(d/1e3, tan(aNIR2/2)*d*2, 'b--')
 rectangle =  patches.Rectangle((1, 1), 1000, 100, edgecolor='red', facecolor='red', linewidth=1, alpha = 0.3)
 ax.add_patch(rectangle)
 grid()
@@ -61,4 +61,5 @@ ax.set_yscale('log')
 ax.legend(['VIS resolution', 'NIR 1 and NIR 2 - x resolution', 'NIR 1 and NIR 2 - y resolution', 'VIS FOV', 'NIR 1 and NIR 2 - x FOV', 'NIR 1 and NIR 2 - y FOV'], loc = 'best')
 ax.set_xlabel('Distance from the target (km)')
 ax.set_ylabel('Image resolution (m/px) and FOV (m)')
-savefig('../../Resolution.png')
+show()
+#savefig('../../Resolution.png')
